b013897179220f48ac0d9f58987d89dc4984aaea
Eric Wang <johnericcw@gmail.com>
2022-02-19 16:15:31 -0600

    Copy paste nextcord guide, but read keys.json

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import json
+
+import nextcord
+
+client = nextcord.Client()
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+token = json.load(open('keys.json'))['token']
+
+client.run(token)
